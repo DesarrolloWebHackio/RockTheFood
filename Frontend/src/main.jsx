a12082feed937ffbd867d8cc0fdd89cb0c50311a
Corocottax <santicorocottax@gmail.com>
2024-11-21 12:22:52 +0100

    FINISH CLASSES

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import UsersProvider from "./providers/usersProvider.jsx";
+import UsersProvider from "./providers/UsersProvider.jsx";
 import IngredientsProvider from "./providers/IngredientsProvider.jsx";
 import RecipesProvider from "./providers/RecipesProvider.jsx";
 
