106fe0ef64dce3b50e466d5644fbde5fa24b3e71
Corocottax <santicorocottax@gmail.com>
2024-11-21 12:27:56 +0100

    FINISH CLASSES

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,18 +2,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import UsersProvider from "./providers/UsersProvider.jsx";
 import IngredientsProvider from "./providers/IngredientsProvider.jsx";
 import RecipesProvider from "./providers/RecipesProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <UsersProvider>
       <IngredientsProvider>
         <RecipesProvider>
           <App />
         </RecipesProvider>
       </IngredientsProvider>
-    </UsersProvider>
   </BrowserRouter>
 );
